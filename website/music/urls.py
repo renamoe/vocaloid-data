bdeda80d202ff0eeb10ba4b064aa3bd1b9f5f77d
renamoe <renamoe@qq.com>
2025-07-01 21:14:24 +0800

    feat(website): 增加列表分页功能, 增加歌词折叠功能, 更改首页为歌曲列表

--- website/music/urls.py
+++ website/music/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mainpage, name="mainpage"),
+    path('', views.song_list, name="mainpage"),
     path('songs/', views.song_list, name="song_list"),
     path('songs/<str:id>', views.song_detail, name="song_detail"),
     path('artists/', views.artist_list, name="artist_list"),
--- website/music/views.py
+++ website/music/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Artist, Song
 
 def mainpage(request):
     return render(request, 'music/mainpage.html')
 
 def song_list(request):
-    songs = Song.objects.all().order_by('name')
+    songs_all = Song.objects.all().order_by('name')
+    paginator = Paginator(songs_all, 15)
+    page_number = request.GET.get('page')
+    songs = paginator.get_page(page_number)
     return render(request, 'music/song_list.html', {'songs': songs})
 
 
@@ -16,11 +20,17 @@ def song_detail(request, id):
 
 
 def artist_list(request):
-    artists = Artist.objects.all().order_by('name')
+    artists_all = Artist.objects.all().order_by('name')
+    paginator = Paginator(artists_all, 15)
+    page_number = request.GET.get('page')
+    artists = paginator.get_page(page_number)
     return render(request, 'music/artist_list.html', {'artists': artists})
 
 
 def artist_detail(request, id):
     artist = get_object_or_404(Artist, artist_id=id)
-    songs = Song.objects.filter(artist__artist_id=id)
+    songs_all = Song.objects.filter(artist__artist_id=id)
+    paginator = Paginator(songs_all, 10)
+    page_number = request.GET.get('page')
+    songs = paginator.get_page(page_number)
     return render(request, 'music/artist_detail.html', {'artist': artist, 'songs': songs})
