f24f37214fc98819ca2d547db4442d308e316fdf
renamoe <renamoe@qq.com>
2025-07-02 20:49:38 +0800

    feat(website): 改用本地图片, 限制图片大小

--- website/music/models.py
+++ website/music/models.py
@@ -6,6 +6,12 @@ class Artist(models.Model):
     img_url = models.CharField(max_length=400)
     description = models.TextField()
     origin_url = models.CharField(max_length=400)
+    
+    @property
+    def get_img_path(self):
+        if len(self.img_url) == 0:
+            return '/media/img_artist/default.jpg'
+        return '/media/img_artist/' + self.artist_id + '.jpg'
 
 class Song(models.Model):
     song_id = models.CharField(max_length=30)
@@ -14,3 +20,7 @@ class Song(models.Model):
     lyric = models.TextField()
     cover_img_url = models.CharField(max_length=400)
     origin_url = models.CharField(max_length=400)
+
+    @property
+    def get_img_path(self):
+        return '/media/img_song_cover/' + self.song_id + '.jpg'
--- website/music/urls.py
+++ website/music/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.song_list, name="mainpage"),
@@ -8,3 +10,6 @@ urlpatterns = [
     path('artists/', views.artist_list, name="artist_list"),
     path('artists/<str:id>', views.artist_detail, name="artist_detail"),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
