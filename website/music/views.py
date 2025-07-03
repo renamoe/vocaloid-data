b6fa111d0c7d998ad338a735966d2104e0b36b91
renamoe <renamoe@qq.com>
2025-07-03 12:48:06 +0800

    feat(website): 添加搜索结果图片、数量、时间展示

--- website/music/views.py
+++ website/music/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.db.models import Q, Case, When, Value, IntegerField
 from .models import Artist, Song
+import time
+
+page_max_lines = 10
 
 def mainpage(request):
     return render(request, 'music/mainpage.html')
 
 def song_list(request):
     songs_all = Song.objects.all().order_by('name')
-    paginator = Paginator(songs_all, 15)
+    paginator = Paginator(songs_all, page_max_lines)
     page_number = request.GET.get('page')
     songs = paginator.get_page(page_number)
     return render(request, 'music/song_list.html', {'songs': songs})
@@ -22,7 +25,7 @@ def song_detail(request, id):
 
 def artist_list(request):
     artists_all = Artist.objects.all().order_by('name')
-    paginator = Paginator(artists_all, 15)
+    paginator = Paginator(artists_all, page_max_lines)
     page_number = request.GET.get('page')
     artists = paginator.get_page(page_number)
     return render(request, 'music/artist_list.html', {'artists': artists})
@@ -31,7 +34,7 @@ def artist_list(request):
 def artist_detail(request, id):
     artist = get_object_or_404(Artist, artist_id=id)
     songs_all = Song.objects.filter(artist__artist_id=id)
-    paginator = Paginator(songs_all, 15)
+    paginator = Paginator(songs_all, page_max_lines)
     page_number = request.GET.get('page')
     songs = paginator.get_page(page_number)
     return render(request, 'music/artist_detail.html', {'artist': artist, 'songs': songs})
@@ -40,6 +43,8 @@ def search(request):
     query = request.GET.get('q', '')
     search_type = request.GET.get('type', 'song')
     page_number = request.GET.get('page')
+
+    start_time = time.time()
 
     if search_type == 'song':
         # 按名称、艺术家、歌词的优先级展示搜索结果
@@ -75,16 +80,21 @@ def search(request):
             )
         ).order_by('match_priority', 'name').distinct()
     
-    paginator = Paginator(results_all, 15)
+    paginator = Paginator(results_all, page_max_lines)
     # 传入参数, 便于翻页时保持其他参数
     query_params = request.GET.copy()
     if 'page' in query_params:
         del query_params['page']
     results = paginator.get_page(page_number)
-    
+
+    search_time = round(time.time() - start_time, 2)
+    search_count = results_all.count()
+
     return render(request, 'search_result.html', {
         'query': query,
         'search_type': search_type,
         'results': results,
-        'query_params': query_params
+        'query_params': query_params,
+        'search_count': search_count,
+        'search_time': search_time
     })
